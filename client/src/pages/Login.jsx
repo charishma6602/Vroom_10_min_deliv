e7405553f141a9931493d81e04452198f9880498
Charishma Darsi <195870737+charishma6602@users.noreply.github.com>
2026-01-26 18:51:09 +0530

    fixed login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
       
       if(response.data.success){
         toast.success(response.data.message)
-        localStorage.setItem('accesstoken',response.data.data.accesstoken)
+        localStorage.setItem('accessToken',response.data.data.accesstoken)
         localStorage.setItem('refreshToken',response.data.data.refreshToken)
 
         const userDetails = await fetchUserDetails()
