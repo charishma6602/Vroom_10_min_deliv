bdccdcb4fcfc780e1b49c865b653580b29632f52
Charishma Darsi <195870737+charishma6602@users.noreply.github.com>
2026-01-26 11:13:39 +0530

    fixed preflight req

--- server/index.js
+++ server/index.js
@@ -83,7 +83,7 @@ app.use((err, req, res, next) => {
 })
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use('/.*/', (req, res) => {
     res.status(404).json({ message: 'Route not found'})
 })
 
