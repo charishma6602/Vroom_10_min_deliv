50238401c818f68c708bf8679b5e6c9ba697b515
Charishma Darsi <195870737+charishma6602@users.noreply.github.com>
2026-01-26 11:04:13 +0530

    fixed preflight reqs

--- server/index.js
+++ server/index.js
@@ -43,7 +43,8 @@ app.use(cors({
 }))
 
 // Handle preflight requests
-app.options('*', cors())
+app.options(/.*/, cors())
+
 
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
